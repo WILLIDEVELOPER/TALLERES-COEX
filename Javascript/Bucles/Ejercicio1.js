928d03d46ac1e00e7942d8b4f26189430fb687e5
WILLIAM SOTO <wilsai1023gmail.com>
2023-01-13 13:54:52 -0500

    Bucles

---
+++ Javascript/Bucles/Ejercicio1.js
@@ -0,0 +1,14 @@
+let nAlumnos = parseInt(prompt("Ingrese la cantidad de alumnos a evaluar"))
+let notas = []
+
+for (let index = 0; index < nAlumnos; index++) {
+    notas.push(prompt(`Ingrese la nota del estudiante: ${index+1}`))
+    if (notas[index] > 3.5) {
+        alert(`El estudiante: ${index+1} Aprobo`)
+    }else{
+        alert(`El estudiante: ${index+1} Reprobo`)
+
+    }
+
+    console.log(`El estudiante: ${index+1} Tiene una nota de: ${notas[index]}`)
+}

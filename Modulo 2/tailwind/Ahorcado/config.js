f8e1969a9fd2c898c86ce554a5dab48e24368e3e
WILLIAM SOTO <wilsai1023gmail.com>
2023-03-02 13:45:12 -0500

    ahorcado

---
+++ Modulo 2/tailwind/Ahorcado/config.js
@@ -0,0 +1,75 @@
+let btn = document.getElementById("btn-search");
+const palabras = ["juego", "leon", "entrenar", "ahorcado", "perro", "negro"];
+
+let palabraRandom = palabras[Math.floor(Math.random() * palabras.length)];
+
+let guiones = palabraRandom.replace(/./g, "_ ");
+const letra = document.getElementById("search");
+let respuesta = document.getElementById("output");
+let contFallos = 0;
+let resultado = document.getElementById("resultado");
+let next = document.getElementById("next");
+let clear = document.getElementById("clear");
+let fallo = true;
+
+const fallos = (fallo) => {
+  let letrica = letra.value;
+  for (let i in palabraRandom) {
+    if (letrica == palabraRandom[i]) {
+      guiones =
+        guiones.substring(0, i * 2) + letrica + guiones.substring(i * 2 + 1);
+      fallo = false;
+    }
+  }
+
+  if (fallo) {
+    contFallos++;
+    document.getElementById("ahorcado").style.backgroundPosition =
+      -(200 * contFallos) + "px 0";
+
+    if (contFallos == 4) {
+      resultado.style.color = "#FF00FF";
+      resultado.innerHTML = "Perdiste";
+    }
+  } else {
+    if (guiones.indexOf("_") < 0) {
+      resultado.style.color = "#FFFF00";
+      resultado.innerHTML = "Ganaste";
+    }
+  }
+};
+
+letra.addEventListener("keypress", (e) => {
+  if (e.keyCode == 13) {
+    fallos(fallo);
+
+    respuesta.innerHTML = guiones;
+    letra.value = "";
+    letra.focus();
+  }
+});
+
+next.addEventListener("click", () => {
+  for (let i = 0; i < palabras.length; i++) {
+    if (palabraRandom == palabras[i]) {
+      palabras.splice(i, 1);
+    }
+  }
+  if (palabras.length > 0) {
+    palabraRandom = palabras[Math.floor(Math.random() * palabras.length)];
+    guiones = palabraRandom.replace(/./g, "_ ");
+    respuesta.innerHTML = guiones;
+    document.getElementById("ahorcado").style.backgroundPosition = 0 + "px 0";
+    letra.value = "";
+    letra.focus();
+    resultado.innerHTML = "";
+  }else{
+    resultado.style.color = "#FFFF00";
+    resultado.innerHTML = "Game Over";
+  }
+
+});
+
+clear.addEventListener("click", () => {
+  window.location.reload();
+});

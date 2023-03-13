52107f2c9d7df9c0eefb145bbe355cedc28d8d2a
WILLIAM SOTO <wilsai1023gmail.com>
2023-03-13 11:46:48 -0500

    ahorcado done

--- Modulo 2/tailwind/Ahorcado/config.js
+++ Modulo 2/tailwind/Ahorcado/config.js
@@ -1,8 +1,6 @@
 let btn = document.getElementById("btn-search");
 const palabras = ["juego", "leon", "entrenar", "ahorcado", "perro", "negro"];
-
 let palabraRandom = palabras[Math.floor(Math.random() * palabras.length)];
-
 let guiones = palabraRandom.replace(/./g, "_ ");
 const letra = document.getElementById("search");
 let respuesta = document.getElementById("output");
@@ -12,31 +10,54 @@ let next = document.getElementById("next");
 let clear = document.getElementById("clear");
 let fallo = true;
 respuesta.innerHTML = guiones;
-let guionesUpdated = []
-let palabraCompleta = document.getElementById("palabraCompleta")
+let guionesUpdated = [];
+let palabraCompleta = document.getElementById("palabraCompleta");
+let inputWord = document.getElementById("wordfull");
+let historial = document.getElementById("historial");
+let modoLetra = document.getElementById("letra");
+// Funciones
 
+palabraCompleta.addEventListener("click", () => {
+  inputWord.hidden = false;
+  letra.hidden = true;
+  inputWord.value = "";
+  inputWord.focus();
+  palabraCompleta.hidden = true;
+  modoLetra.hidden = false;
+});
 
+modoLetra.addEventListener("click", () => {
+  inputWord.hidden = true;
+  letra.hidden = false;
+  letra.value = "";
+  letra.focus();
+  palabraCompleta.hidden = false;
+  modoLetra.hidden = true;
+});
 
-const fallos = (fallo) => {
-  let letrica = letra.value;
-  for (let i in palabraRandom) {
-    if (letrica == palabraRandom[i]) {
-      guiones =
-        guiones.substring(0, i * 2) + letrica + guiones.substring(i * 2 + 1);
+inputWord.addEventListener("keypress", (e) => {
+  if (e.keyCode == 13) {
+    if (palabraRandom.includes(inputWord.value)) {
       fallo = false;
+      fallos(fallo, inputWord.value);
+    }else{
+      if (inputWord.value == palabraRandom) {
+        guiones = inputWord.value;
+        fallo = false;
+      } else {
+        fallo = true;
+      }
     }
+      
+    valFallos(fallo);
+    historial.innerHTML += inputWord.value + ", ";
+    respuesta.innerHTML = guiones;
+    inputWord.value = "";
+    inputWord.focus();
   }
+});
 
-
-  for (let i = 0; i < guionesUpdated.length; i++) {
-    if (guionesUpdated[i].includes(letrica)) {
-      fallo = true
-      break;
-    }
-  }
-  
-
-
+const valFallos = (fallo) => {
   if (fallo) {
     contFallos++;
     document.getElementById("ahorcado").style.backgroundPosition =
@@ -52,15 +73,32 @@ const fallos = (fallo) => {
       resultado.innerHTML = "Ganaste";
     }
   }
+};
 
-  
+const fallos = (fallo, letra) => {
+  for (let i in palabraRandom) {
+    if (letra == palabraRandom[i]) {
+      guiones =
+        guiones.substring(0, i * 2) + letra + guiones.substring(i * 2 + 1);
+      fallo = false;
+    }
+  }
+
+  for (let i = 0; i < guionesUpdated.length; i++) {
+    if (guionesUpdated[i].includes(letra)) {
+      fallo = true;
+      break;
+    }
+  }
+
+  valFallos(fallo);
 };
 
 letra.addEventListener("keypress", (e) => {
   if (e.keyCode == 13) {
-    guionesUpdated.push(guiones)
-    fallos(fallo);
-
+    guionesUpdated.push(guiones);
+    fallos(fallo, letra.value);
+    historial.innerHTML += letra.value + ", ";
     respuesta.innerHTML = guiones;
     letra.value = "";
     letra.focus();
@@ -68,6 +106,8 @@ letra.addEventListener("keypress", (e) => {
 });
 
 next.addEventListener("click", () => {
+  contFallos = 0;
+  fallo = true;
   for (let i = 0; i < palabras.length; i++) {
     if (palabraRandom == palabras[i]) {
       palabras.splice(i, 1);
@@ -81,11 +121,10 @@ next.addEventListener("click", () => {
     letra.value = "";
     letra.focus();
     resultado.innerHTML = "";
-  }else{
+  } else {
     resultado.style.color = "#FFFF00";
     resultado.innerHTML = "Game Over";
   }
-
 });
 
 clear.addEventListener("click", () => {
